3cfca71cf1ba8d9c93dfa15f7ae136b431983446
Sonal Raj <b22me062@iitj.ac.in>
2025-06-22 15:03:17 +0530

    added read me

--- utils/pdf_utils.py
+++ utils/pdf_utils.py
@@ -1,9 +1,9 @@
-import PyPDF2
-
+from langchain_community.document_loaders import PyPDFLoader
+ 
 def extract_text_from_pdf(pdf_path):
+    loader = PyPDFLoader(pdf_path)
+    documents = loader.load()
     text = ""
-    with open(pdf_path, 'rb') as file:
-        reader = PyPDF2.PdfReader(file)
-        for page in reader.pages:
-            text += page.extract_text()
+    for doc in documents:
+        text += doc.page_content
     return text
